47da0abfad08afd5c3efc58294c3dc566548a0b8
Jingjing Wu <112022982+jingjingwu1998@users.noreply.github.com>
2025-01-24 12:08:14 -0500

    Update slurm.sh

--- slurm.sh
+++ slurm.sh
@@ -14,23 +14,5 @@
 conda activate py3.7
 # pyega3 -c 1 -cf /home/80030577/ega-download-client/pyega3/config/default_credential_file.json fetch EGAF00004726920 --output-dir /share/lab_teng/trainee/JingjingWu/EBV/spaindata
 # pyega3 -c 5 -cf /home/80030577/ega-download-client/pyega3/config/default_credential_file.json fetch EGAF00004726921 --output-dir /share/lab_teng/trainee/JingjingWu/EBV/spaindata
-# Define variables
-OUTPUT_DIR="/share/lab_teng/trainee/JingjingWu/EBV/spaindata/"
-CREDENTIALS_FILE="/home/80030577/ega-download-client/pyega3/config/default_credential_file.json"
 
-# Loop through the file IDs from 921 to 975
-for FILE_ID in $(seq -f "EGAF00004726%02g" 921 975); do
-    echo "Starting download for ${FILE_ID}..."
-
-    # Run pyega3 command
-    pyega3 -c 1 -cf ${CREDENTIALS_FILE} fetch ${FILE_ID} --output-dir ${OUTPUT_DIR}
-
-    # Check if the file was successfully downloaded
-    if [ $? -eq 0 ]; then
-        echo "Download of ${FILE_ID} completed successfully and saved to ${OUTPUT_DIR}."
-    else
-        echo "Download of ${FILE_ID} failed. Please check the logs for more details."
-    fi
-
-    echo "--------------------------------------------"
-done
+for id in $(seq 26922 26975); do pyega3 -c 5 -cf /home/80030577/ega-download-client/pyega3/config/default_credential_file.json fetch EGAF000047$id --output-dir /share/lab_teng/trainee/JingjingWu/EBV/spaindata; done
